ea59c28e132399b8ccec8db3edb773fc49934e09
Tosya451 <phoenixed.451@gmail.com>
2017-09-12 22:12:58 +0300

    Start changing IBinder targeting to Messenger.

--- app/src/main/java/com/example/batrakov/servicetask/MainActivity.java
+++ app/src/main/java/com/example/batrakov/servicetask/MainActivity.java
@@ -4,9 +4,13 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.os.IBinder;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,13 +18,15 @@ import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
+    static final int MSG_CHANGE_STR = 1;
     boolean mBound = false;
 
-    MyService mService;
+    Messenger mService = null;
     EditText text;
     Button button;
     String str;
     Intent intent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,17 +40,47 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                sayHello();
                 str = text.getText().toString();
-                mService.setString(str);
             }
         });
 
 
         intent = new Intent(this, MyService.class);
-        startService(intent);
-        bindService(intent, mConnection, BIND_AUTO_CREATE);
+//        startService(intent);
+//        bindService(intent, mConnection, BIND_AUTO_CREATE);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
 
     }
+
+
+    public void sayHello() {
+        if (!mBound) return;
+        // Create and send a message to the service, using a supported 'what' value
+        Message msg = Message.obtain(null, MyService.MSG_SAY_HELLO, 0, 0);
+        try {
+            mService.send(msg);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_CHANGE_STR:
+                    text.setText("Test");
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+
+    final Messenger mMessenger = new Messenger(new IncomingHandler());
 
     @Override
     protected void onStart() {
@@ -78,19 +114,23 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onServiceConnected(ComponentName className, IBinder service) {
-            MyService.LocalBinder binder = (MyService.LocalBinder) service;
-            mService = binder.getService();
-            str = mService.getString();
-            text.setText(str);
+            // This is called when the connection with the service has been
+            // established, giving us the object we can use to
+            // interact with the service.  We are communicating with the
+            // service using a Messenger, so here we get a client-side
+            // representation of that from the raw IBinder object.
+            mService = new Messenger(service);
+            mBound = true;
             Log.i("Service1", "connected");
             Log.i("Service1", str +"_");
-            mBound = true;
 
         }
 
         @Override
         public void onServiceDisconnected(ComponentName arg0) {
-            Log.i("Service1", "disconnected");
+            // This is called when the connection with the service has been
+            // unexpectedly disconnected -- that is, its process crashed.
+            mService = null;
             mBound = false;
         }
     };
--- app/src/main/java/com/example/batrakov/servicetask/MyService.java
+++ app/src/main/java/com/example/batrakov/servicetask/MyService.java
@@ -3,13 +3,51 @@ package com.example.batrakov.servicetask;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
 import android.util.Log;
+import android.widget.Toast;
 
 
 public class MyService extends Service {
 
-    private final IBinder mBinder = new LocalBinder();
+
+    /** Command to the service to display a message */
+    static final int MSG_SAY_HELLO = 1;
+
+    /**
+     * Handler of incoming messages from clients.
+     */
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_SAY_HELLO:
+                    Toast.makeText(getApplicationContext(), "hello!", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+
+    /**
+     * Target we publish for clients to send messages to IncomingHandler.
+     */
+    final Messenger mMessenger = new Messenger(new IncomingHandler());
+
+    /**
+     * When binding to the service, we return an interface to our messenger
+     * for sending messages to the service.
+     */
+    @Override
+    public IBinder onBind(Intent intent) {
+        Toast.makeText(getApplicationContext(), "binding", Toast.LENGTH_SHORT).show();
+        return mMessenger.getBinder();
+    }
+
     String str;
     int i = 0;
 
@@ -44,17 +82,6 @@ public class MyService extends Service {
         MyService getService() {
             return MyService.this;
         }
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        return START_STICKY;
-    }
-
-    @Override
-    public IBinder onBind(Intent intent) {
-
-        return mBinder;
     }
 
     /** methods for clients */
